612c849796c84345c5cf71de63eb8d05e80613ea
Gokul Krishna <gokulkrishnatu11@gmail.com>
2025-03-03 16:43:59 +0600

    initial build fix

--- src/component/CreateWallet/CreateWallet.js
+++ src/component/CreateWallet/CreateWallet.js
@@ -1,50 +1,15 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import "./CreateWallet.css"
-
-
-// const CreateWallet = () => {
-//   const [wallet, setWallet] = useState(null);
-
-//   const handleCreateWallet = async () => {
-//     try {
-//       const response = await axios.post("http://localhost:5000/create_wallet");
-//       console.log('response: ', response.data);
-//       setWallet(response.data);
-//     } catch (error) {
-//       console.error("Error creating wallet:", error);
-//     }
-//   };
-
-//   return (
-//     <div className="Container">
-//       <h2>Create Wallet</h2>
-//       <button onClick={handleCreateWallet}>Create New Wallet</button>
-//       {wallet && (
-//         <div className="wallet-detials">
-//           <p>Mnemonic: {wallet.words}</p>
-//           <p>Address: {wallet.address}</p>
-//           <p>Private Key: {wallet.private_key}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CreateWallet;
-
 import React, { useState } from "react";
 import axios from "axios";
 import "./CreateWallet.css";
 
 const CreateWallet = ({ onWalletCreate }) => {
-  const [wallet, setWallet] = useState(null);
+  // If you no longer need to store wallet state locally, you can remove this line
+  // const [wallet, setWallet] = useState(null);
 
   const handleCreateWallet = async () => {
     try {
       const response = await axios.post("http://localhost:5000/create_wallet");
-      setWallet(response.data);
-      onWalletCreate(response.data); // Pass data to parent
+      onWalletCreate(response.data); // Pass data to parent component
     } catch (error) {
       console.error("Error creating wallet:", error);
     }
@@ -57,6 +22,5 @@ const CreateWallet = ({ onWalletCreate }) => {
     </div>
   );
 };
-
 
 export default CreateWallet;
